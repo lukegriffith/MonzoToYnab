9c9b62d67508b136d6e60b0978793d6af9b3fd06
Luke M Griffith <lukemgriffith@gmail.com>
2018-04-09 16:16:19 +0100

    removing policy.

--- tf/main.tf
+++ tf/main.tf
@@ -25,32 +25,9 @@ resource "aws_iam_role" "function_role" {
   assume_role_policy = "${data.aws_iam_policy_document.function_role_policy.json}"
 }
 
-data "aws_iam_policy_document" "bucket_policy" {
-  # Bucket policy trumps IAM policy, needs to explicitly be allowed access for management.
-  statement {
-    sid = "labmdaRoleGet"
-
-    effect = "Allow"
-
-    principals = {
-      type        = "AWS"
-      identifiers = ["${aws_iam_role.function_role.arn}"]
-    }
-
-    actions = [
-      "s3:Get*",
-      "s3:List*",
-    ]
-
-    resources = [
-      "arn:aws:s3:::lglambdaconfig",
-    ]
-  }
-}
 
 resource "aws_s3_bucket" "labmdaFunctionConfig" {
   bucket     = "lglambdaconfig"
   acl        = "private"
-  policy     = "${data.aws_iam_policy_document.bucket_policy.json}"
   depends_on = ["aws_iam_role.function_role"]
 }
